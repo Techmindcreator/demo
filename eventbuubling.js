a8a8143e99570f944f7d57bab754d61b136875f2
Arun <aruntechmindcreator@gmail.com>
2026-06-29 09:37:24 +0530

    print statement is updated

--- eventbuubling.js
+++ eventbuubling.js
@@ -1,13 +1,14 @@
-const parent=document.getElementById("parent");
-const Child=document.getElementById("child");
-parent.addEventListener("click",()=>{
-    console.log("parent called");
-})
+const parent = document.getElementById("parent");
+const Child = document.getElementById("child");
+parent.addEventListener("click", () => {
+  console.log("parent called");
+});
 // Child.addEventListener("click",(event)=>{
 //     console.log("Child is called")
 //     event.stopPropagation();
 // })
-const ul=document.getElementById("ul");
-ul.addEventListener("click",(event)=>{
-console.log(event.target.innerText);
-})
+const ul = document.getElementById("ul");
+ul.addEventListener("click", (event) => {
+  console.log(event.target.innerText);
+  console.log(event.target.innerText);
+});
